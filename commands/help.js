f9d3fd6b4f3c73e6f55fae0da7e60fed27e4bbc6
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-18 13:57:25 -0700

    Update remaining reference to the config file

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,10 @@
-const { prefix } = require('../config.json');
-
 module.exports = {
 	name: 'help',
 	description: 'Instructions on how to use Cactus',
 	execute(message) {
 		message.channel.send(
 			`It was wise of you to ask for help, ${message.author.username}.\n\n
-		My prefix is ${prefix} - based on my current instructions, any message intended to issue a command to me must begin with this prefix.\n\n
+		My prefix is ${process.env.PREFIX} - based on my current instructions, any message intended to issue a command to me must begin with this prefix.\n\n
 		Here is the list of my current commands:\n
 		${message.client.commands.map(c => c.name).join('\n')}`);
 	},
